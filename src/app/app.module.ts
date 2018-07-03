d91533d902a290c7c12a72a1d7ff16ad43fed3e6
stephenKise <stephenrkise@gmail.com>
2018-07-03 15:52:28 -0400

    Add Bootstrap to the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,9 @@ import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
 
 import { AppComponent } from './app.component'
-import { UserComponent } from './components/user/user.component';
+import { UserComponent } from './components/user/user.component'
 import { UsersComponent } from './components/users/users.component'
+import { NavbarComponent } from './components/navbar/navbar.component'
 
 
 @NgModule({
@@ -11,6 +12,7 @@ import { UsersComponent } from './components/users/users.component'
     AppComponent,
     UserComponent,
     UsersComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule
