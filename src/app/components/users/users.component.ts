fe085f4a0d28ca1f7204e972baa1dad51c4a6c90
stephenKise <stephenrkise@gmail.com>
2018-07-03 06:02:45 -0400

    Work with *ngFor, list dummy data in the DOM

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { User } from '../../models/User'
 
 @Component({
   selector: 'app-users',
@@ -6,11 +7,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
+  users: User[]
 
   constructor() { }
 
   ngOnInit() {
-    console.log('[UsersComponent] Initialized!')
+    this.users = [
+      {
+        firstName: 'John',
+        lastName: 'Doe',
+        age: 25,
+        address: {
+          street: '123 The Post Road Apt. K',
+          city: 'Springfield',
+          state: 'OH'
+        }
+      },
+      {
+        firstName: 'James',
+        lastName: 'Cameron',
+        age: 22,
+        address: {
+          street: '935 School St',
+          city: 'Lynn',
+          state: 'MA'
+        }
+      },
+      {
+        firstName: 'Carrie',
+        lastName: 'Johnson',
+        age: 19,
+        address: {
+          street: '1100 OH-347',
+          city: 'East Liberty',
+          state: 'OH'
+        }
+      }
+    ]
+    this.addUser({
+      firstName: 'David',
+      lastName: 'Jackson',
+      age: 30,
+      address: {
+        street: '100 Lagonda Ave',
+        city: 'Springfield',
+        state: 'OH'
+      }
+    })
   }
 
+  addUser(user: User): void {
+    this.users.push(user)
+  }
 }
