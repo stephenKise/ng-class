91af981cd8c8fafaf838b7f5694c617ec6772730
stephenKise <stephenrkise@gmail.com>
2018-07-03 05:40:27 -0400

    Create new users component through CLI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
 
 import { AppComponent } from './app.component'
-import { UserComponent } from './components/user/user.component'
+import { UserComponent } from './components/user/user.component';
+import { UsersComponent } from './components/users/users.component'
 
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
+    UsersComponent,
   ],
   imports: [
     BrowserModule
